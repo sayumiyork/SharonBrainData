3efc3b0323e448aa8d9a52c779094707fa9c016a
sayumiyork <syork@ndm.edu>
2025-11-07 22:13:48 -0500

    Add mouse RNA-seq related functions
    * Add all mouse related functions originally from rna-seq-project-work.Rmd
    * Added a minimum level of required documentation
    
    Future improvements could include adding more detailed documentation and having the package automatically load DESeq2 and ggplot2 when loaded. Currently, you must manually load the DESeq2 library before calling an object from this library.

---
+++ R/plotAcrossMice.R
@@ -0,0 +1,25 @@
+
+#' plotAcrossMice
+#'
+#' Creates a figure plotting the gene expression using filtered TD-data from the Sharon et al. (2019) dataset across mice.
+#'
+#'
+#' @param x A string containing a valid Mouse Genome Informatics (MGI) ID that is found in the dataset
+#' @return A figure plotting the gene expression of filtered TD-data from the Sharon et al. (2019) dataset across mice
+
+
+plotAcrossMice <- function( x ) {
+  df <- data.frame(counts(brain)[x,], brain_metadata$bio_sample, row.names = row.names(brain_metadata))
+  colnames(df) <- c("counts", "bio_sample")
+  df$bio_sample <- fct_relevel( factor( df$bio_sample), "PFC_TD_1_1", "PFC_TD_1_2", "PFC_TD_1_3","PFC_TD_1_4",
+                                "PFC_TD_2_1", "PFC_TD_2_2", "PFC_TD_2_3","PFC_TD_2_4",
+                                "PFC_TD_3_1","PFC_TD_3_2","PFC_TD_3_3","PFC_TD_3_4","PFC_TD_3_5","PFC_TD_3_6","PFC_TD_3_7",
+                                "Striatum_TD_1_1","Striatum_TD_1_2","Striatum_TD_1_3","Striatum_TD_1_4",
+                                "Striatum_TD_2_1","Striatum_TD_2_2","Striatum_TD_2_3","Striatum_TD_2_4",
+                                "Striatum_TD_3_1","Striatum_TD_3_2","Striatum_TD_3_3","Striatum_TD_3_4","Striatum_TD_3_6","Striatum_TD_3_7")
+  ggplot( df ) +
+    geom_bar( aes( bio_sample, counts ), stat="identity" ) +
+    ggtitle(x)+
+    theme_bw() +
+    theme( axis.text.x = element_text( angle=90, vjust=0.5 ) )
+}
---
+++ R/runClusterProfilerMouse.R
@@ -0,0 +1,15 @@
+#' runClusterProfilerMouse
+#'
+#' Performs a clusterProfiler analysis on mouse data using KEGG categories from org.Mm.eg.db.
+#'
+#' @param x A data frame containing the results of a DESeq2 analysis
+#' @return The results of an over representation test - what genes are significantly up/down regulated - and their correlating KEGG categories
+#' runClusterProfilerMouse()
+
+
+runClusterProfilerMouse <- function (x) {
+  ids <- bitr( x$GeneID, "ENSEMBL", "ENTREZID", "org.Mm.eg.db" )
+  kegg <- enrichKEGG(ids$ENTREZID, "mmu", keyType="ncbi-geneid")
+  kegg@result$Description <- sub( " - Mus musculus \\(house mouse\\)", "", kegg@result$Description )
+  return(kegg)
+}
