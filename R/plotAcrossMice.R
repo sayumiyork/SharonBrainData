424f4a300dd384d073fda47c7cae8d797a504d0f
sayumiyork <syork@ndm.edu>
2025-11-07 23:13:56 -0500

    Add export to functions to enable use outside of package
    *Added @export to .R scripts and NAMESPACE (automatically)
    *Change git config file to always save as LF line endings

--- R/plotAcrossMice.R
+++ R/plotAcrossMice.R
@@ -6,6 +6,7 @@
 #'
 #' @param x A string containing a valid Mouse Genome Informatics (MGI) ID that is found in the dataset
 #' @return A figure plotting the gene expression of filtered TD-data from the Sharon et al. (2019) dataset across mice
+#' @export
 
 
 plotAcrossMice <- function( x ) {
